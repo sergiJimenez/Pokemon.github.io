f5a13181fd69811045a28963ec22f1dcca1deb1c
PerezSJ <sperezj2021@cepnet.net>
2021-10-17 21:25:08 +0200

    Modify1

--- index.php
+++ index.php
@@ -11,6 +11,39 @@
 <body>
     <h1>
         <?php
+        $pokedex =
+            $pokemon1 = array(
+                "Number" => "001",
+                "Name" => "Bulbasur",
+                "Region" => "Hoen",
+                "Type" => "Plant, Poison",
+                "Height" => "70",
+                "Weight" => "6.9",
+                "Evolution" => "Without Evolution",
+                "Picture" => "001.png"
+            );
+
+        $pokemon2 = array(
+            "Number" => "002",
+            "Name" => "Ivysur",
+            "Region" => "Hoen",
+            "Type" => "Plant, Poison",
+            "Height" => "100",
+            "Weight" => "13",
+            "Evolution" => "First Evolution",
+            "Picture" => "002.png"
+        );
+
+        $pokemon3 = array(
+            "Number" => "004",
+            "Name" => "Charmander",
+            "Region" => "Jotho",
+            "Type" => "Fire",
+            "Height" => "60",
+            "Weight" => "8.5",
+            "Evolution" => "Without Evolution",
+            "Picture" => "003.png"
+        );
         ?>
     </h1>
 </body>
