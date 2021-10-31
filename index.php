e658fd9164c3e4e47f3251c728f05f7858fe510b
PerezSJ <sperezj2021@cepnet.net>
2021-10-31 09:33:15 +0100

    introducing Bootstrap and linking

--- index.php
+++ index.php
@@ -10,9 +10,12 @@ include "php_library/pokedex.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <script src="bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
 </head>
 
 <body>
+
     <h1>
         <?php
         //Creamos una pokedex vacia:
