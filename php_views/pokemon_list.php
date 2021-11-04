136de46474ee1f5f7d47dea53473ea04e7dc1b5a
PerezSJ <sperezj2021@cepnet.net>
2021-11-04 14:30:14 +0100

    add images and create css's

---
+++ php_views/pokemon_list.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Searching Pokemon</title>
+    <link rel="stylesheet" href="../style/searchPokemon.css">
+    <link rel="stylesheet" href="../bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <script src="../bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
+</head>
+
+<body class="body_pokemon">
+    <?php include 'php_partials/menu.php'; ?>
+
+    <div class="container-fluid p-4">
+        <br>
+
+        <div class="row row-cols-1 row-cols-md-5 g-4">
+            <div class="col d-flex align-items-stretch">
+                <div class="card border-secondary">
+                    <img src="media/img/001.png" class="card-img-top" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">001 - Bulbasur</h5>
+                        <span class="badge bg-warning text-dark">Planta</span>
+                        <span class="badge bg-warning text-dark">Veneno</span>
+                    </div>
+                    <div class="card-footer text-end">
+                        <button href="#" type="button" class="btn btn-outline-danger"><i class="bi bi-trash"></i></button>
+                        <button href="#" type="button" class="btn btn-outline-primary"><i class="bi bi-pencil"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-flex align-items-stretch">
+                <div class="card border-secondary">
+                    <img src="media/img/002.png" class="card-img-top" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">002 - Ivysaur</h5>
+                        <span class="badge bg-warning text-dark">Planta</span>
+                        <span class="badge bg-warning text-dark">Veneno</span>
+                    </div>
+                    <div class="card-footer text-end">
+                        <button href="#" type="button" class="btn btn-outline-danger"><i class="bi bi-trash"></i></button>
+                        <button href="#" type="button" class="btn btn-outline-primary"><i class="bi bi-pencil"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-flex align-items-stretch">
+                <div class="card border-secondary">
+                    <img src="media/img/003.png" class="card-img-top" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">003 - Venusaur</h5>
+                        <span class="badge bg-warning text-dark">Planta</span>
+                        <span class="badge bg-warning text-dark">Veneno</span>
+                    </div>
+                    <div class="card-footer text-end">
+                        <button href="#" type="button" class="btn btn-outline-danger"><i class="bi bi-trash"></i></button>
+                        <button href="#" type="button" class="btn btn-outline-primary"><i class="bi bi-pencil"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-flex align-items-stretch">
+                <div class="card border-secondary">
+                    <img src="media/img/004.png" class="card-img-top" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">004 - Charmander</h5>
+                        <span class="badge bg-warning text-dark">Fuego</span>
+                    </div>
+                    <div class="card-footer text-end">
+                        <button href="#" type="button" class="btn btn-outline-danger"><i class="bi bi-trash"></i></button>
+                        <button href="#" type="button" class="btn btn-outline-primary"><i class="bi bi-pencil"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-flex align-items-stretch">
+                <div class="card border-secondary">
+                    <img src="media/img/005.png" class="card-img-top" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">005 - Charmeleon</h5>
+                        <span class="badge bg-warning text-dark">Fuego</span>
+                    </div>
+                    <div class="card-footer text-end">
+                        <button href="#" type="button" class="btn btn-outline-danger"><i class="bi bi-trash"></i></button>
+                        <button href="#" type="button" class="btn btn-outline-primary"><i class="bi bi-pencil"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-flex align-items-stretch">
+                <div class="card border-secondary">
+                    <img src="media/img/006.png" class="card-img-top" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">006 - Charizard</h5>
+                        <span class="badge bg-warning text-dark">Fuego</span>
+                    </div>
+                    <div class="card-footer text-end">
+                        <button href="#" type="button" class="btn btn-outline-danger"><i class="bi bi-trash"></i></button>
+                        <button href="#" type="button" class="btn btn-outline-primary"><i class="bi bi-pencil"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-flex align-items-stretch">
+                <div class="card border-secondary">
+                    <img src="media/img/007.png" class="card-img-top" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">007 - Squirtle</h5>
+                        <span class="badge bg-warning text-dark">Agua</span>
+                    </div>
+                    <div class="card-footer text-end">
+                        <button href="#" type="button" class="btn btn-outline-danger"><i class="bi bi-trash"></i></button>
+                        <button href="#" type="button" class="btn btn-outline-primary"><i class="bi bi-pencil"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-flex align-items-stretch">
+                <div class="card border-secondary">
+                    <img src="media/img/008.png" class="card-img-top" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">008 - Wartortle</h5>
+                        <span class="badge bg-warning text-dark">Agua</span>
+                    </div>
+                    <div class="card-footer text-end">
+                        <button href="#" type="button" class="btn btn-outline-danger"><i class="bi bi-trash"></i></button>
+                        <button href="#" type="button" class="btn btn-outline-primary"><i class="bi bi-pencil"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-flex align-items-stretch">
+                <div class="card border-secondary">
+                    <img src="media/img/009.png" class="card-img-top" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">009 - Blastoise</h5>
+                        <span class="badge bg-warning text-dark">Agua</span>
+                    </div>
+                    <div class="card-footer text-end">
+                        <button href="#" type="button" class="btn btn-outline-danger"><i class="bi bi-trash"></i></button>
+                        <button href="#" type="button" class="btn btn-outline-primary"><i class="bi bi-pencil"></i></button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="position-fixed text-dark position-absolute bottom-0 end-0 m-5" style="height: 10px; width: 10px;">
+        <a href="pokemon.php" type="button" class="btn bg-warning text-dark rounded-circle"><i class="bi bi-plus-lg"></i></a>
+    </div>
+
+</body>
+
+</html>
