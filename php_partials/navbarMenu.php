b422361c737152f3a0ee0ce0238fa61adb35af4c
PerezSJ <sperezj2021@cepnet.net>
2021-11-05 09:35:05 +0100

    add comments

--- php_partials/navbarMenu.php
+++ php_partials/navbarMenu.php
@@ -19,7 +19,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary text-light" id="navbar">
     <div class="container-fluid">
         <!-- INDEX LINK --> <a class="navbar-brand" href="../index.php">
-            <img src="/Pokemon.github.io/media/Pokedex.png" width="30" height="24" class="d-inline-block align-text-top">
+            <img src="/Pokemon.github.io/media/Pokedex.png" width="30" height="24" class="d-inline-block align-text-top"><!-- ABSOLUTE LINK (/ALL_ROUTE) -->
             Pokedex
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -32,7 +32,7 @@
                         Datos Maestros
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="/Pokemon.github.io/php_views/pokemon_list.php">Pokemons</a></li>
+                        <li><a class="dropdown-item" href="/Pokemon.github.io/php_views/pokemon_list.php">Pokemons</a></li><!-- ABSOLUTE LINK (/ALL_ROUTE) -->
                     </ul>
                 </li>
             </ul>
