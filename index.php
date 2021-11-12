03c7f77d6dd68a9cdcad70c20b89704a8d4b6c1f
PerezSJ <sperezj2021@cepnet.net>
2021-11-12 20:07:23 +0100

    index compiled

--- index.php
+++ index.php
@@ -25,21 +25,21 @@
         $pokedex = array();
 
         //Creamos 1 pokemon:
-        $pokemon = createPokemon("001", "Bulbasur", "Hoen", "Plant, Poison", "70", "6.9", "Without Evolution", "001.png");
+        $pokemon = createPokemon("001", "Bulbasur", "Hoen", $Type = ("Plant, Poison"), "70", "6.9", "Without Evolution", "001.png");
         //Añadimos el pokemon creado a la pokedex:
         addPokemon($pokemon, $pokedex);
         //EXTRA = Mostramos el pokemon añadido:
         showPokemon($pokemon);
 
         //Creamos 2 pokemon:
-        $pokemon = createPokemon("002", "Ivysur", "Hoen", "Plant, Poison", "100", "13", "First Evolution", "002.png");
+        $pokemon = createPokemon("002", "Ivysur", "Hoen", $Type = ("Plant, Poison"), "100", "13", "First Evolution", "002.png");
         //Añadimos el pokemon creado a la pokedex:
         addPokemon($pokemon, $pokedex);
         //EXTRA = Mostramos el pokemon añadido:
         showPokemon($pokemon);
 
         //Creamos 3 pokemon:
-        $pokemon = createPokemon("004", "Charmander", "Jotho", "Fire", "60", "8.5", "Without Evolution", "004.png");
+        $pokemon = createPokemon("004", "Charmander", "Jotho", $Type = ("Fire"), "60", "8.5", "Without Evolution", "004.png");
         //Añadimos el pokemon creado a la pokedex:
         addPokemon($pokemon, $pokedex);
         //EXTRA = Mostramos el pokemon añadido:
@@ -49,14 +49,11 @@
         showPokedex($pokedex);
 
         //Elimina el pokemon:
-        dropPokemon($pokedex, $pokemon);
+        dropPokemon($pokedex, "001");
 
-        //Volvemos a mostrar toda la pokedex:
-        showPokedex($pokedex);
-
-        //En el ejercicio no sale pero para mostrar que funciona ejecutaré las dos funciones no indicadas:
         //Modifica un pokemon:
-        modifyPokemon($pokedex, "Bulbasur", "0011", "Mega Bulbasur", "Jotho", "Plant, Poison, Fire", "170", "655.89", "With Evolution", "009.png");
+        $pokemonMOD = createPokemon("001", "Mega Bulbasur", "Hoen", $Type = ("Plant, Poison"), "70", "6.9", "Without Evolution", "001.png");
+        modifyPokemon($pokedex, $pokemonMOD);
         showPokedex($pokedex);
 
         //Busca un pokemon:
